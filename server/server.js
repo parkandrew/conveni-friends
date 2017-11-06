abfca8e7bdd64b9c6b9f0d083e7de7aea5fb1b89
JJ Estacio <jjestacio@EVA2.local>
2017-11-06 13:13:23 -0800

    mysql setup + demo

--- server/server.js
+++ server/server.js
@@ -1,4 +1,12 @@
 import express from "express";
+import mysql from "mysql";
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'cs130_project',
+});
 
 const server = express();
 const PORT = 3000;
@@ -8,7 +16,15 @@ server.get('/', (req, res) => {
 });
 
 server.listen(PORT, () => {
+    connection.connect();
     console.log(`Listening on ${PORT}`);
+
+    connection.query('SELECT * FROM Test', (err, rows) => {
+        if (err)
+            throw err;
+
+        console.log(rows);
+    });
 });
 
 export default server;
