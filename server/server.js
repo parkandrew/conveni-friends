f455b23e3dbee5183e14c44d5089edbd46c88109
JJ Estacio <jjestacio@EVA2.local>
2017-11-09 17:07:43 -0800

    switching

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import http from "http";
 import mysql from "mysql";
 
-const connection = mysql.createConnection({
+const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: '',
@@ -13,20 +13,28 @@ const app = express();
 const server = http.Server(app);
 const PORT = 3000;
 
+// TODO: Validation of request params. Although, I'm thinking that Validation
+// should be done client side, aka before the make the request, so we'll discuss
+// that
+
 app.get('/', (req, res) => {
     res.send("Test GET request");
 });
 
+app.get('/v1/request/create', (req, res) => {
+    // In practice, will use req.params, but easier to modify the URL
+    // query params during development
+    const { username, title, description } = req.query;
+    const { location, timeStart, timeEnd } = req.query;
+
+    db.query(`INSERT INTO TABLE `)
+
+    res.send(username);
+});
+
 server.listen(PORT, () => {
-    connection.connect();
+    db.connect();
     console.log(`Listening on ${PORT}`);
-
-    connection.query('SELECT * FROM Test', (err, rows) => {
-        if (err)
-            throw err;
-
-        console.log(rows);
-    });
 });
 
 export default server;
