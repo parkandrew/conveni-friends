8812e7612c3f16879e2796b3f41243d7b53a1b3b
JJ Estacio <jjestacio@EVA2.local>
2017-11-13 14:28:49 -0800

    finished get all user requests

--- server/server.js
+++ server/server.js
@@ -13,6 +13,9 @@ const app = express();
 const server = http.Server(app);
 const PORT = 3000;
 
+// TODO: We should split up the routes into separate files .. one for
+// user endpoints, request endpoints, etc.
+
 // TODO: I think we could create our own function that handles the db queries
 // so that we can define a way to handle errors
 
@@ -21,30 +24,46 @@ app.get('/', (req, res) => {
 });
 
 app.post('/v1/request/:requestId/complete', (req, res) => {
-    const { username, time } = req.query;
+    const { userId, time } = req.query;
     const { requestId } = req.params;
 
     const query = `UPDATE Request SET completed=${time} `
                 + `WHERE requestId=${requestId}`;
 
-    db.query(query, (error, results) => console.log(error || "Success"));
+    db.query(query, (error, results) => {
+        console.log(error || "Success")
+    });
 });
 
-app.get('/v1/request/create', (req, res) => {
+app.post('/v1/request/create', (req, res) => {
     // In practice, will use req.params, but easier to modify the URL
     // query params during development
 
     // We're also currently assuming that the frontend passes strings like
     // "string" rather than string.
-    const { username, title, description } = req.query;
+    const { userId, title, description } = req.query;
     const { location, timeStart, timeEnd } = req.query;
 
     // TODO: Gotta figure out a more cleaner way to do our queries ..
     const query = `INSERT INTO Request(requesterId,title,location,description,`
-                + `timeStart,timeEnd) VALUES(${username},${title},`
+                + `timeStart,timeEnd) VALUES(${userId},${title},`
                 + `${location},${description},${timeStart},${timeEnd})`;
 
-    db.query(query, (error, results) => console.log(error || "Success"));
+    db.query(query, (error, results) => {
+        console.log(error || "Success")
+    });
+});
+
+app.get('/v1/user/:userId/requests', (req, res) => {
+    const { userId } = req.params;
+
+    const query = `SELECT * FROM Request `
+                + `WHERE requesterId="${userId}" or providerId="${userId}"`;
+
+    db.query(query, (error, results) => {
+        console.log(error || "Success");
+        res.send(results || error);
+    });
 });
 
 server.listen(PORT, () => {
