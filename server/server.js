50b836b144501179365e9e481756e1b78f5ec1dd
Michael Xiong <mmxiong@ucla.edu>
2017-11-13 17:09:37 -0800

    add delete request call to API

--- server/server.js
+++ server/server.js
@@ -21,6 +21,8 @@ app.get('/', (req, res) => {
     res.send("Test GET request");
 });
 
+// Example call:
+// http://localhost:3000/v1/request/create/username=test&title=testtitle&description=testdescription&location=testlocation&timeStart=%220000-00-00%2000:00:00%22&timeEnd=%220000-00-00%2000:00:00%22
 app.get('/v1/request/create', (req, res) => {
     // In practice, will use req.params, but easier to modify the URL
     // query params during development
@@ -35,6 +37,27 @@ app.get('/v1/request/create', (req, res) => {
                 + `timeStart,timeEnd) VALUES(${username},${title},`
                 + `${location},${description},${timeStart},${timeEnd})`;
 
+    db.query(query, (error, results) => {
+        if (error) {
+            console.log(error.message);
+        } else {
+            console.log("SUCCESS!");
+            console.log(results);
+        }
+    });
+});
+
+// Example call:
+// http://localhost:3000/v1/request/1/delete?username="test"
+app.post('/v1/request/:request_id/delete', (req, res) => {
+    const requestId = req.params.request_id;
+    const { username } = req.query;
+
+    // TODO: Gotta figure out a more cleaner way to do our queries ..
+    const query = `DELETE FROM Request `
+                + `WHERE requestId=${requestId} AND requesterId=${username}`;
+    console.log(query)
+    
     db.query(query, (error, results) => {
         if (error) {
             console.log(error.message);
