7901bf487c86c2b426e4e4941eb6bb0b3e626b85
Michael Xiong <mmxiong@ucla.edu>
2017-11-13 18:44:53 -0800

    update plus signs for nit and update username to userId for all routes

--- server/server.js
+++ server/server.js
@@ -22,20 +22,20 @@ app.get('/', (req, res) => {
 });
 
 // Example call:
-// http://localhost:3000/v1/request/create/username=test&title=testtitle&description=testdescription&location=testlocation&timeStart=%220000-00-00%2000:00:00%22&timeEnd=%220000-00-00%2000:00:00%22
+// http://localhost:3000/v1/request/create?userId="test"&title="testtitle"&description="testdescription"&location="testlocation"&timeStart="%2017-03-03%2011:11:11"&timeEnd="2017-04-04%2011:11:011"
 app.get('/v1/request/create', (req, res) => {
     // In practice, will use req.params, but easier to modify the URL
     // query params during development
 
     // We're also currently assuming that the frontend passes strings like
     // "string" rather than string.
-    const { username, title, description } = req.query;
+    const { userId, title, description } = req.query;
     const { location, timeStart, timeEnd } = req.query;
 
     // TODO: Gotta figure out a more cleaner way to do our queries ..
-    const query = `INSERT INTO Request(requesterId,title,location,description,`
-                + `timeStart,timeEnd) VALUES(${username},${title},`
-                + `${location},${description},${timeStart},${timeEnd})`;
+    const query = `INSERT INTO Request(requesterId,title,location,description,` +
+                  `timeStart,timeEnd) VALUES(${userId},${title},` +
+                  `${location},${description},${timeStart},${timeEnd})`;
 
     db.query(query, (error, results) => {
         if (error) {
@@ -48,14 +48,13 @@ app.get('/v1/request/create', (req, res) => {
 });
 
 // Example call:
-// http://localhost:3000/v1/request/1/delete?username="test"
+// http://localhost:3000/v1/request/1/delete?userId="test"
 app.post('/v1/request/:request_id/delete', (req, res) => {
     const requestId = req.params.request_id;
-    const { username } = req.query;
+    const { userId } = req.query;
 
-    // TODO: Gotta figure out a more cleaner way to do our queries ..
     const query = `DELETE FROM Request `
-                + `WHERE requestId=${requestId} AND requesterId=${username}`;
+                + `WHERE requestId=${requestId} AND requesterId=${userId}`;
     console.log(query)
     
     db.query(query, (error, results) => {
