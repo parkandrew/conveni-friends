aa6215c97985b3b92efdbb9187403245e07cb92d
Brandon Liu <bliu7654@gmail.com>
2017-11-16 17:12:26 -0800

    Add tests

--- client/App.test.js
+++ client/App.test.js
@@ -7,3 +7,18 @@ it('renders without crashing', () => {
   const rendered = renderer.create(<App />).toJSON();
   expect(rendered).toBeTruthy();
 });
+
+it('renders login screen', () => {
+	const rendered = renderer.create(<Login />).toJSON();
+	expect(rendered).toBeTruthy();
+})
+
+it('does not accept username input with special characters', () => {
+	const username = 'brandon/';
+	expect(validateUsername(username).to.equal(false));
+})
+
+it('does accept username input without special characters', () => {
+	const username = 'brandon23';
+	expect(validateUsername(username).to.equal(false));
+})
