d11b86247bdd9adcc9bb475378267e009874bb99
Brandon Liu <bliu7654@gmail.com>
2017-11-19 22:10:44 -0800

    change console.log messages

--- client/app/Common/HamburgerMenu.js
+++ client/app/Common/HamburgerMenu.js
@@ -6,8 +6,8 @@ export default class HamburgerMenu extends Component {
         return(
             <View style={styles.container}>
                 <Text>User ID</Text>
-                <Button title='Request List' onPress={()=>console.log()}/>
-                <Button title='Settings' onPress={()=>console.log()} />
+                <Button title='Request List' onPress={()=>console.log("request list")}/>
+                <Button title='Settings' onPress={()=>console.log("settings")} />
                 <Button title='Logout'
                     onPress={() => {this.props.logout();}}/>
             </View>
