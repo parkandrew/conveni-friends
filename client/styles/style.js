202558f95c84b3d1adaa81f773675df2e31ccd6a
Brandon Liu <bliu7654@gmail.com>
2017-11-26 18:21:31 -0800

    add basic global styles for vars

--- client/styles/style.js
+++ client/styles/style.js
@@ -1,8 +1,15 @@
 import { StyleSheet } from 'react-native';
 
 // consts
-  let screenWidth = 350;
-  let fontSize = 16;
+let screenWidth = 350;
+let font__large = 40;
+let font__medium = 20;
+let font__small = 16;
+let fontSize = 16;
+
+let $white = '#FFFFFF';
+let $blue__primary = '#ADD8E6';
+let $blue__secondary = '#3D95DA';
 
 const styles = StyleSheet.create({
 
@@ -17,7 +24,7 @@ const styles = StyleSheet.create({
 		height: 65,
 		paddingRight: 6,
 		paddingLeft: 6,
-		backgroundColor: '#ADD8E6',
+		backgroundColor: $blue__primary,
 	},
 	cardLeft: {
 		flex: 0.8,
@@ -25,10 +32,10 @@ const styles = StyleSheet.create({
 		justifyContent: 'center',
 	},
 	cardTitle: {
-		fontSize: 20,
+		fontSize: font__medium,
 	},
 	cardSubTitle: {
-		fontSize: 16,
+		fontSize: font__small,
 		marginTop: 4
 	},
 	cardRight: {
@@ -37,17 +44,17 @@ const styles = StyleSheet.create({
 		paddingLeft: 6,
 	},
 	cardDistance: {
-		fontSize: 16
+		fontSize: font__small
 	},
 
 // HomeScreen
     homeTitle: {
-      fontSize: 40,
+      fontSize: font__large,
       color: 'red'
     },
     homeContainer: {
       flex: 1,
-      backgroundColor: '#fff',
+      backgroundColor: $white,
       alignItems: 'center',
       justifyContent: 'center',
     },
@@ -58,12 +65,12 @@ const styles = StyleSheet.create({
 
 // LoginScreen
     loginTitle: {
-      fontSize: 40,
+      fontSize: font__large,
       color: 'red'
       },
     loginContainer: {
       flex: 1,
-      backgroundColor: '#fff',
+      backgroundColor: $white,
       alignItems: 'center',
       justifyContent: 'center',
     },
@@ -71,23 +78,23 @@ const styles = StyleSheet.create({
 
 // NearbyRequests
     nearbyTitle: {
-      fontSize: 20,
+      fontSize: font__medium,
       color: 'red'
     },
     nearbyContainer: {
       flex: 1,
-      backgroundColor: '#fff',
+      backgroundColor: $white,
     },
 
 // SignupScreen
 
     signupTitle: {
-      fontSize: 20,
+      fontSize: font__medium,
       color: 'red'
     },
     signupContainer: {
       flex: 1,
-      backgroundColor: '#fff',
+      backgroundColor: $white,
       alignItems: 'center',
       justifyContent: 'center',
     },
@@ -100,18 +107,18 @@ const styles = StyleSheet.create({
     },
     dtpDatePicker: {
       width: screenWidth/2.4,
-      backgroundColor: '#FFF',
-      height: fontSize + 16,
-      fontSize: fontSize,
+      backgroundColor: $white,
+      height: font__small + 16,
+      fontSize: font__small,
       padding: 5,
       paddingLeft: 10,
-      borderColor: '#3D95DA',
+      borderColor: $blue__secondary,
       borderWidth: 2,
       borderRadius: 6,
       overflow: 'hidden'
     },
     dtpInputHeader: {
-      fontSize: fontSize,
+      fontSize: font__small,
       marginBottom: 1
     },
 
@@ -122,28 +129,28 @@ const styles = StyleSheet.create({
 		alignItems: 'flex-start',
 	},
 	formInput: {
-		backgroundColor:'#FFF',
+		backgroundColor:$white,
 		padding: 10,
 		width: screenWidth,
-		height: fontSize + 26,
-		fontSize: fontSize,
-		borderColor: '#3D95DA',
+		height: font__small + 26,
+		fontSize: font__small,
+		borderColor: $blue__secondary,
 		borderWidth: 2,
 		borderRadius: 6
 	},
 	formInputHeader: {
-		fontSize: fontSize,
+		fontSize: font__small,
 		marginBottom: 1
 	},
 	formMultiLine: {
-		height: fontSize * 6,
+		height: font__small * 6,
 	},
 
 // MakeRequestScreen/MakeRequest
 
 	makeContainer: {
 		flex: 1,
-		backgroundColor: '#ADD8E6',
+		backgroundColor: $blue__primary,
 		alignItems: 'center',
 		paddingTop: 20,
 	},
