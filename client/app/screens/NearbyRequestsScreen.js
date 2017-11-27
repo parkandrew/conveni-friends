f02fc77a6f013f1650cf22ba936f526a8ed1b952
Brandon Liu <bliu7654@gmail.com>
2017-11-26 18:04:45 -0800

    Add request list component and its list separator. refactor nearbyreq screen to use thsi component

--- client/app/screens/NearbyRequestsScreen.js
+++ client/app/screens/NearbyRequestsScreen.js
@@ -1,39 +1,26 @@
 import React from 'react';
-import { FlatList, ListView, Alert, Text, View, TextInput, Button } from 'react-native';
-import styles from 'client/styles/style';
-import Card from 'client/app/components/Card'
+import { StyleSheet, FlatList, ListView, Alert, Text, View, TextInput, Button } from 'react-native';
+// import styles from 'client/styles/style';
+import RequestListComponent from 'client/app/components/RequestListComponent';
 
 export default class NearbyRequests extends React.Component {
-    static navigationOptions = {
-        //headerLeft: null,
-    }
-    constructor(props) {
-        super(props);
-        // const dataSource = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
-        // this.state = {
-        //     sessionKey: '',
-        //     currentLocation: '',
-        //     dataSource: dataSource.cloneWithRows(['row 1', 'row 2']),
-        // };
-        //this._getNearbyRequests = this._getNearbyRequests.bind(this)
+	static navigationOptions = {
+		//headerLeft: null,
 	}
-    render() {
+	constructor(props) {
+		super(props);
+	}
+	render() {
 		const { navigate } = this.props.navigation;
-		const data = {
+		const data = [
+			{ key: 'In-n-out', distance: '0.6 mi', startTime: '9:00am', endTime: '10:00pm' },
+			{ key: 'Ride to Ralph\'s', distance: '1.4 mi', startTime: '1:00am', endTime: '2:00am'},
+			{ key: 'Need to use a printer for essayasdfasdf asdfasdf sad', distance: '1000 mi', startTime: '9:00am', endTime: '10:00pm'}
+		];
 
-		}
-        return (
-            // <ListView style={styles.nearbyContainer}
-            //     dataSource={this.state.dataSource}
-            //     renderRow={(rowData) => <Text>{rowData}</Text>}>
-            // </ListView>
-			<FlatList
-				data={[{key: 'a', info:'asdf'}, {key: 'b', info:'fdsa'}, {key: 'c', info:'frontend'}]}
-				renderItem={({item}) =>
-					<Card data={item} />
-					// <Text>{item.key}{item.info}</Text>
-				}
-			/>
-        );
-    }
+		const ITEM_HEIGHT = 400;
+		return (
+			<RequestListComponent data={data}/>
+		);
+	}
 }
