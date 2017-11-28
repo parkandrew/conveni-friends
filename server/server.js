1a8d24e433770337bd28d432fa99843272fec219
Andrew Park <parkandrew95@gmail.com>
2017-11-28 00:11:39 -0800

    when returning list of requests, also return distance

--- server/server.js
+++ server/server.js
@@ -382,6 +382,32 @@ app.get('/v1/user/:userId/requests', (req, res) => {
     });
 });
 
+// Credit for the following function goes to:
+// http://www.movable-type.co.uk/scripts/latlong.html
+// User: Deduplicator
+// https://stackoverflow.com/questions/27928/calculate-distance-between-two-latitude-longitude-points-haversine-formula
+// Modified to return miles instead of kilometers
+function getDistanceFromLatLonInMiles(lat1,lon1,lat2,lon2) {
+  var R = 6371; // Radius of the earth in km
+  var dLat = deg2rad(lat2-lat1);  // deg2rad below
+  var dLon = deg2rad(lon2-lon1); 
+  var a = 
+    Math.sin(dLat/2) * Math.sin(dLat/2) +
+    Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
+    Math.sin(dLon/2) * Math.sin(dLon/2)
+    ; 
+
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+  var d = R * c; // Distance in km
+  var miles = d * 0.621371
+  
+  return miles;
+}
+
+function deg2rad(deg) {
+  return deg * (Math.PI/180)
+}
+
 /**
  * Called when a user looks up nearby requests.
  *
@@ -414,7 +440,21 @@ app.get('/v1/requests/all', (req, res) => {
         }
         else {
             console.log("Success");
-            res.status(HttpStatus.OK).send(results);
+
+            var retValue = [];
+
+            for (var i = 0; i < results.length; i++) {
+                console.log(results[i]);
+                console.log("HIHIHIEFHIHGROIJGROIWGEWOIGHWEOGHOIEWG");
+                console.log(getDistanceFromLatLonInMiles(latitude, longitude, results[i].latitude, results[i].longitude));
+                var dist = getDistanceFromLatLonInMiles(latitude, longitude, results[i].latitude, results[i].longitude);
+                retValue.push({
+                    request : results[i],
+                    distance: dist
+                });
+            }
+
+            res.status(HttpStatus.OK).send(retValue);
         }
     });
 });
