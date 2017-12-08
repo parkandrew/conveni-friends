6561e3c854257908d6df3edfe2ed2936c0898e6f
JJ Estacio <jjestacio@EVA2.local>
2017-12-08 13:15:39 -0800

    accepted/completed buttons

--- client/app/screens/RequestDetailsScreen.js
+++ client/app/screens/RequestDetailsScreen.js
@@ -1,26 +1,68 @@
 import React from 'react';
-import { View, Text, ScrollView, TouchableOpacity } from 'react-native';
+import axios from 'axios';
+import moment from 'moment';
+
+import { Button, View, Text, ScrollView, TouchableOpacity } from 'react-native';
 import RequestInfoLine from 'client/app/components/RequestInfoDetails';
 import CustomButton from 'client/app/components/CustomButton';
 import styles from 'client/styles/style';
+import config from 'client/config';
+
 export default class RequestDetailsScreen extends React.Component {
 	static navigationOptions = {
 		title: 'Request Details',
 	}
 
+	constructor(props) {
+		super(props);
+
+		this.state = {
+		};
+
+		this.accept = this.accept.bind(this);
+		this.complete = this.complete.bind(this);
+	}
+
+	accept() {
+		const { requesterId, requestId } = this.props.navigation.state.params.data;
+
+		axios.post(`${config.API_URL}/v1/request/${requestId}/accept`, {
+			userId: requesterId,
+			time: moment().format('YYYY-MM-DD HH:MM:SS')
+		});
+	}
+
+	complete() {
+		const { requesterId, requestId } = this.props.navigation.state.params.data;
+
+		axios.post(`${config.API_URL}/v1/request/${requestId}/complete`, {
+			userId: requesterId,
+			time: moment().format('YYYY-MM-DD HH:MM:SS')
+		});
+	}
+
 	render() {
+		const { title, location, details } = this.props.navigation.state.params.data;
+		const { startTime, endTime } = this.props.navigation.state.params.data;
+		const { accepted, confirmed, completed } = this.props.navigation.state.params.data;
+
 		return (
 			<View style={{ flex: 1 }}>
 				<ScrollView style={reqStyles.makeContainer}>
 					<View style={reqStyles.makeInputView}>
-						<RequestInfoLine primary={'Request'} secondary={this.props.navigation.state.params.data.title} />
-						<RequestInfoLine primary={'Location'} secondary={this.props.navigation.state.params.data.location} />
-						<RequestInfoLine primary={'Start Time'} secondary={this.props.navigation.state.params.data.startTime} />
-						<RequestInfoLine primary={'End Time'} secondary={this.props.navigation.state.params.data.endTime} />
-						<Text style={reqStyles.key}>Details: <Text style={reqStyles.value}>{this.props.navigation.state.params.data.details}</Text></Text>
+						<RequestInfoLine primary={'Request'} secondary={title} />
+						<RequestInfoLine primary={'Location'} secondary={location} />
+						<RequestInfoLine primary={'Start Time'} secondary={startTime} />
+						<RequestInfoLine primary={'End Time'} secondary={endTime} />
+						<Text style={reqStyles.key}>Details: <Text style={reqStyles.value}>{details}</Text></Text>
 					</View>
 					<CustomButton style={reqStyles.buttonContainer} text={'Message Requester'}/>
 				</ScrollView>
+
+				{ accepted
+					? <Button title="Accept" onPress={() => this.accept()} />
+					: <Button title="Complete" onPress={() => this.complete()} />
+				}
 			</View>
 		);
 	}
