9927860b327e1613e122be0d3fad34b86f5d0014
JJ Estacio <jjestacio@EVA2.local>
2017-12-11 19:28:44 -0800

    new bug introduced, will come back to

--- client/config.js
+++ client/config.js
@@ -1,4 +1,8 @@
 export default config = {
-    API_URL: 'https://conveni-friends.herokuapp.com',
-    WS_URL: 'ws://conveni-friends.herokuapp.com',
+    API_URL: 'http://e3b1c502.ngrok.io',
+    WS_URL: 'ws://e3b1c502.ngrok.io',
 };
+// export default config = {
+//     API_URL: 'https://conveni-friends.herokuapp.com',
+//     WS_URL: 'ws://conveni-friends.herokuapp.com',
+// };
--- server/server.js
+++ server/server.js
@@ -12,11 +12,20 @@ import WebSocket from "ws";
 
 const pool = mysql.createPool({
   connectionLimit : 20,
-  host: 'us-cdbr-iron-east-05.cleardb.net',
-  user: 'beffa2b11a15f1',
-  password: '704f96be',
-  database: 'heroku_f4bd3eb0d7b7de1',
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'cs130_project',
 });
+
+//
+// const pool = mysql.createPool({
+//   connectionLimit : 20,
+//   host: 'us-cdbr-iron-east-05.cleardb.net',
+//   user: 'beffa2b11a15f1',
+//   password: '704f96be',
+//   database: 'heroku_f4bd3eb0d7b7de1',
+// });
 
 const dbQuery = (query, callback) => {
     pool.getConnection((err, connection) => {
@@ -442,7 +451,6 @@ app.get('/v1/user/:userId/requests', (req, res) => {
                 + `WHERE BINARY requesterId="${userId}" OR BINARY providerId="${userId}"`;
 
     dbQuery(query, (error, results) => {
-      console.log(results);
         if (error) {
             console.log(error);
             return res.status(HttpStatus.INTERNAL_SERVER_ERROR)
@@ -566,11 +574,11 @@ app.get('/v1/requests/all', (req, res) => {
  * @param {string} userId2 - The id of the second user in message session
  */
 app.post('/v1/message/session/create', (req, res) => {
-    const { userId1, userId2 } = req.query;
+    const { userId1, userId2 } = req.body;
 
      const query1 = `SELECT * FROM MessageSession ` +
-                    `WHERE (userId1=${userId1} AND userId2 = ${userId2}) ` +
-                    `OR (userId1=${userId2} AND userId2 = ${userId1})`;
+                    `WHERE (userId1='${userId1}' AND userId2 = '${userId2}') ` +
+                    `OR (userId1='${userId2}' AND userId2 = '${userId1}')`;
 
     dbQuery(query1, (error, results) => {
     	if (error) {
@@ -580,22 +588,22 @@ app.post('/v1/message/session/create', (req, res) => {
 
         // Found MessageSession
         } else if (results.length == 1){
-            res.status(HttpStatus.OK).send(results[0]);
+            res.status(HttpStatus.OK).json(JSON.stringify(results[0]));
 
         // Create MessageSession
         } else {
             const query2 = `INSERT INTO MessageSession(userId1, userId2) ` +
-                           `VALUES(${userId1},${userId2})`;
+                           `VALUES('${userId1}','${userId2}')`;
 
             dbQuery(query2, (error, results) => {
                 if (error) {
                     console.log(error);
                     return res.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                        .send({ message: "Internal server error." });
+                        .json({ message: "Internal server error." });
                 }
                 else {
                     console.log("Success");
-                    res.status(HttpStatus.OK).send(results[0]);
+                    res.status(HttpStatus.OK).json(JSON.stringify(results[0]));
                 }
             });
         }
@@ -617,6 +625,7 @@ app.post('/v1/message/session/create', (req, res) => {
  */
 app.get('/v1/message/session/:messageSessionId', (req, res) => {
     const { messageSessionId } = req.params;
+
     // NOTE: user._id and user.name is always equal to senderId (this is how GiftedChat
     // knows who sent what).
     //
